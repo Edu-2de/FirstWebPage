9dc7540db867c7a694190ba1fb1c97ccf3858579
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-06-22 14:43:44 -0300

    chore: add prettier config

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,18 +1,17 @@
-import { Route, Routes } from "react-router-dom"
-import LayoutMain from "./pages/LayoutMain"
-import PageComponent from "./pages/PageComponent"
-import PageHome from "./pages/PageHome"
+import { Route, Routes } from 'react-router-dom'
+import LayoutMain from './components/layout/LayoutMain'
+import PageComponent from './pages/PageComponent'
+import PageHome from './pages/PageHome'
 
 function App() {
-  return(
+  return (
     <Routes>
-      <Route element={<LayoutMain/>}>
-        <Route path="/" element={<PageHome/>} />
+      <Route element={<LayoutMain />}>
+        <Route path="/" element={<PageHome />} />
       </Route>
-      <Route path="/components" element={<PageComponent/>} />
+      <Route path="/components" element={<PageComponent />} />
     </Routes>
   )
-
 }
 
 export default App
