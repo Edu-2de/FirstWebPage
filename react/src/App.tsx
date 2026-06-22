62816a05deec8600eb737c5e80bf56f361772bd6
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-06-22 10:16:38 -0300

    feat: add initial structure of react app

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,35 +1,18 @@
-import Text from "./components/Text"
-
+import { Route, Routes } from "react-router-dom"
+import LayoutMain from "./pages/LayoutMain"
+import PageComponent from "./pages/PageComponent"
+import PageHome from "./pages/PageHome"
 
 function App() {
-  return (
-    <div className='w-full h-screen flex flex-col items-center gap-6 p-6 bg-background '>
-      <div className='w-full flex flex-row gap-6 justify-center items-center'>
-          <Text size="sm" color="black">Teste</Text>
-          <Text size="sm" color="gray">Teste</Text>
-          <Text size="sm" color="primary">Teste</Text>
-          <Text size="sm" color="secondary">Teste</Text>
-      </div>
-      <div className='w-full flex flex-row gap-6 justify-center items-center'>
-          <Text size="md" color="black">Teste</Text>
-          <Text size="md" color="gray">Teste</Text>
-          <Text size="md" color="primary">Teste</Text>
-          <Text size="md" color="secondary">Teste</Text>
-      </div>
-      <div className='w-full flex flex-row gap-6 justify-center items-center'>
-          <Text size="lg" color="black">Teste</Text>
-          <Text size="lg" color="gray">Teste</Text>
-          <Text size="lg" color="primary">Teste</Text>
-          <Text size="lg" color="secondary">Teste</Text>
-      </div>
-      <div className='w-full flex flex-row gap-6 justify-center items-center'>
-          <Text size="xl" color="black">Teste</Text>
-          <Text size="xl" color="gray">Teste</Text>
-          <Text size="xl" color="primary">Teste</Text>
-          <Text size="xl" color="secondary">Teste</Text>
-      </div>
-    </div>
+  return(
+    <Routes>
+      <Route element={<LayoutMain/>}>
+        <Route path="/" element={<PageHome/>} />
+      </Route>
+      <Route path="/components" element={<PageComponent/>} />
+    </Routes>
   )
+
 }
 
 export default App
