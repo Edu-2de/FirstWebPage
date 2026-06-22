6210a1dbb753da48553c3263657dd45e2d7fc361
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-06-22 10:20:51 -0300

    feat: add core component

---
+++ react/src/components/Container.tsx
@@ -0,0 +1,32 @@
+import type { ComponentProps } from "react"
+import { tv, type VariantProps } from "tailwind-variants"
+
+const containerVariants = tv({
+  base : `w-full flex lg:flex-row flex-col lg:justify-center items-center xl:px-64 xl:py-15 p-6 gap-6`,
+  variants:{
+    containerColor:{
+      primary: 'bg-primary',
+      secondary: 'bg-secondary',
+      background: 'bg-background',
+      none: 'bg-transparent'
+    }
+  },
+  defaultVariants:{
+    containerColor: 'none',
+  }
+
+})
+
+type ContainerVariantsProps = VariantProps<typeof containerVariants>
+
+interface ContainerProps extends ComponentProps<'div'>, ContainerVariantsProps{}
+
+export default function Container({className, containerColor,  children, ...props}: ContainerProps){
+
+  return(
+    <div className={containerVariants({containerColor, className})} {...props}>
+      {children}
+    </div>
+  )
+
+}
